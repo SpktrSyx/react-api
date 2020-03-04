b59dc74889311a8e8c905771c6bee090549d058b
SpktrSyx <ballandras.cindy@gmail.com>
2020-03-04 16:08:09 +0100

    add banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import DataContainer from './containers/DataContainer';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { StarWarsLogo } from './images';
 
 const App = () =>
 <Container>
+  <h1 className="title d-flex flex-column justify-content-center align-items-center mt-5 mb-5">
+    The
+    <Image src={StarWarsLogo} />
+    Encyclopedia
+  </h1>
   <BrowserRouter>
     <Switch>
-    <Route exact path="/:resource(\w+)" component={DataContainer} />
+      <Route exact path="/:resource(\w+)" component={DataContainer} />
       <Route exact path="/:resource(\w+)/:id(\d+)" component={DataContainer} />
     </Switch>
   </BrowserRouter>
---
+++ src/images/index.js
@@ -0,0 +1,1 @@
+export { default as StarWarsLogo } from './logoStarWars.jpg';
